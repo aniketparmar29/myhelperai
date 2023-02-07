8e685fc983aab416643f637c5443b584ff27f803
aniketparmar29 <aniketparmar068@gmail.com>
2023-02-07 23:37:41 +0530

    ASD

--- server/server.js
+++ server/server.js
@@ -35,8 +35,8 @@ app.post('/', async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0.7,
-            max_tokens: 400,
+            temperature: 0,
+            max_tokens: 3000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
