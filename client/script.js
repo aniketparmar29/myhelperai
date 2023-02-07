ed91e48d7d6b2199c5138f901115a062225f6b07
aniketparmar29 <aniketparmar068@gmail.com>
2023-02-07 23:33:34 +0530

    ASD

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   console.log(data.get('prompt'))
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
-  const response = await fetch("http://localhost:5000",{
+  const response = await fetch("https://mychatgtp.onrender.com/",{
     method:'POST',
     headers:{
       'Content-Type':'application/json'
