99da033b02e67d68f35e50bdf2a6ca4379404291
aniketparmar29 <aniketparmar068@gmail.com>
2023-02-09 12:51:06 +0530

    AS

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.post('/', async (req, res) => {
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 3000,
+            max_tokens: 500,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
